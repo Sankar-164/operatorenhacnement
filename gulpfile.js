c0378ab06d7377590f3d2f5b7a2df889779462a4
Sankar-164 <121457102+Sankar-164@users.noreply.github.com>
2023-12-20 15:45:22 +0530

    commit2

--- gulpfile.js
+++ gulpfile.js
@@ -18,31 +18,31 @@ gulp.task('clean', function(cb) {
 });
 
 gulp.task('browserify', ['clean'], function(cb) {
-    return gulp.src('./src/PathFinding.js')
+    return gulp.src('src\PathFinding.js')
     .pipe(browserify({ standalone: 'PF' }))
     .pipe(rename('pathfinding-browserified.js'))
     .pipe(gulp.dest('./lib/'), cb);
 });
 
 gulp.task('uglify', ['browserify'], function(cb) {
-    return gulp.src('./lib/pathfinding-browserified.js')
+    return gulp.src('lib/pathfinding-browserified.js')
     .pipe(uglify())
     .pipe(rename('pathfinding-browser.min.js'))
-    .pipe(gulp.dest('./lib/'), cb);
+    .pipe(gulp.dest('lib/'), cb);
 });
 
 gulp.task('scripts', ['clean', 'browserify', 'uglify'], function(cb) {
-    return gulp.src(['./src/banner', './lib/pathfinding-browserified.js'])
+    return gulp.src(['src/banner', 'lib/pathfinding-browserified.js'])
     .pipe(concat('pathfinding-browser.js'))
-    .pipe(gulp.dest('./lib/'), cb);
+    .pipe(gulp.dest('lib/'), cb);
 });
 
 gulp.task('compile', ['scripts'], function() {
-    del('./lib/pathfinding-browserified.js');
+    del('lib/pathfinding-browserified.js');
 });
 
 gulp.task('test', function () {
-    return gulp.src('./test/**/*.js', {read: false})
+    return gulp.src('test/**/*.js', {read: false})
         .pipe(mocha({reporter: 'spec', bail: true, globals: { should: require('should') }}));
 });
 
@@ -51,7 +51,7 @@ gulp.task('bench', function() {
 });
 
 gulp.task('lint', function() {
-  return gulp.src('./src/**/*.js')
+  return gulp.src('src/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter('fail'));
@@ -66,9 +66,9 @@ gulp.task('release', ['compile'], function(cb) {
       //default is patch
       default: 0
     }, function (result) {
-      var f = jsonfile.readFileSync('./package.json');
+      var f = jsonfile.readFileSync('package.json');
       f.version = semver.inc(f.version, result.bumpType);
-      jsonfile.writeFileSync('./package.json', f);
+      jsonfile.writeFileSync('package.json', f);
 
       shell.exec('git add .');
       shell.exec('git commit -m "Bumping version to ' + f.version + '"');
@@ -79,8 +79,8 @@ gulp.task('release', ['compile'], function(cb) {
 
       shell.exec('git clone https://github.com/imor/pathfinding-bower.git release');
       process.chdir('release');
-      fs.writeFileSync('pathfinding-browser.js', fs.readFileSync('../lib/pathfinding-browser.js'));
-      fs.writeFileSync('pathfinding-browser.min.js', fs.readFileSync('../lib/pathfinding-browser.min.js'));
+      fs.writeFileSync('pathfinding-browser.js', fs.readFileSync('lib/pathfinding-browser.js'));
+      fs.writeFileSync('pathfinding-browser.min.js', fs.readFileSync('lib/pathfinding-browser.min.js'));
 
       f = jsonfile.readFileSync('bower.json');
       f.version = semver.inc(f.version, result.bumpType);
